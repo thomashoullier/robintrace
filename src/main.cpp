0665bbdfed44c8878e8c1174773fcb63c37af6a0
Thomas HOULLIER <pro@houllier.net>
2022-09-18 20:45:12 +0200

    added the ray class

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,15 @@
 #include <iostream>
+#include "ray.h"
 
 int main(){
-  std::cout << "Hello World" << std::endl;
+  ray ray1;
+  ray1.x = 0.0;
+
+
+  std::cout << "Hello World " << ray1.y << std::endl;
+
+  // TODO:
+  // * Create very basic objects such as point in 3d, and lmn vector in 3d.
+  //   this simplifies greatly the expression of computations.
+  //   A ray is a point and lmn vector.
 }
---
+++ src/ray/ray.h
@@ -0,0 +1,14 @@
+#ifndef RAY_H
+#define RAY_H
+
+class ray {
+  public:
+    double x;
+    double y;
+    double z;
+    double l;
+    double m;
+    double n;
+};
+
+#endif // RAY_H
