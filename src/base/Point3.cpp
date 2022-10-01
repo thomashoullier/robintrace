ab92525e731353b840c896e5f5a77d7e3f16e04b
Thomas HOULLIER <pro@houllier.net>
2022-10-01 17:35:19 +0200

    printer and additional constructor for Point3

--- src/base/Point3.cpp
+++ src/base/Point3.cpp
@@ -1,7 +1,17 @@
 #include "base/Point3.h"
 
 Point3::Point3 () {
-  x = 0;
-  y = 0;
-  z = 0;
+  // Constructor.
+  x = 0; y = 0; z = 0;
+}
+
+Point3::Point3 (double _x, double _y, double _z) {
+  // Construct + initialize.
+  x = _x; y = _y; z = _z;
+}
+
+std::ostream& operator<< (std::ostream &out, Point3 const& p) {
+  // Printer method
+  out << "Point3(" << p.x << ", " << p.y << ", " << p.z << ")";
+  return out;
 }
--- src/main.cpp
+++ src/main.cpp
@@ -2,15 +2,21 @@
 #include "ray/ray.h"
 #include "shape/plane.h"
 #include "shape/sphere.h"
+#include "base/Point3.h"
 
 int main(){
   ray ray1;
+  ray1.p.x = 1;
+
   plane plane1;
   sphere sphere1(4.5);
 
   plane1.intersect(ray1);
 
-  ray1.p.x = 1;
+  Point3 p(1.0, 2.0, 3.0);
+  Point3 p2(p);
+  p.x = 5.0;
+  std::cout << p2 << std::endl;
 
   std::cout << "Ray z: " << ray1.p.z << std::endl;
   std::cout << "Ray code: " << ray1.code << std::endl;
