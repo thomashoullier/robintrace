1ad29fd3284aeb98213310f3eeb499ed825d856d
Thomas HOULLIER <pro@houllier.net>
2023-04-24 22:11:40 +0200

    documented Vec3.h, added a precision to the poaky documentation on direction cosines

--- src/poaky/src/base/Vec3.cpp
+++ src/poaky/src/base/Vec3.cpp
@@ -1,9 +1,11 @@
 #include "base/Vec3.h"
 
+/**
+ * The component n is built automatically from (l, m) and the specified sign
+ * so as to obtain a unit vector.
+ * */
 Vec3 Vec3_lm (double l, double m, bool nsign) {
-  // Construct and initialize a unit Vec3. The component n
-  // is built automatically from l, m and its specified sign.
-  // zsign is true for +1, false for -1.
+  // nsign: True for +1, False for -1.
   double n = std::sqrt(1 - l*l - m*m);
   n = (nsign) ? n : - n;
   Vec3 v (l, m, n);
--- src/poaky/src/base/Vec3.h
+++ src/poaky/src/base/Vec3.h
@@ -1,11 +1,22 @@
+/**
+ * @file Vec3.h
+ */
+
 #ifndef VEC3_H
 #define VEC3_H
 
 #include <cmath>
 #include <Eigen/Dense>
 
+/** A 3D vector. */
 typedef Eigen::Vector3d Vec3;
 
+/** @brief
+ * Constructor of a unit Vec3 from direction cosines (l, m) and the sign of n.
+ * @param l Direction cosine in the x direction.
+ * @param m Direction cosine in the y direction.
+ * @param nsign Sign of the direction cosine in the z direction. True for
+ * positive, False for negative. */
 Vec3 Vec3_lm (double l, double m, bool nsign);
 
 #endif //VEC3_H
