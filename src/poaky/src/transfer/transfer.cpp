8caead1a5c5fba9b66ae8d80aa775d109273488a
Thomas HOULLIER <pro@houllier.net>
2023-04-30 10:57:17 +0200

    poaky is documented in doxygen

--- src/poaky/src/transfer/transfer.cpp
+++ src/poaky/src/transfer/transfer.cpp
@@ -1,17 +1,28 @@
 #include "transfer.h"
 
-/* Constructors */
+/** The defaults are:
+ * - \f$ B = B^{-1} = I \f$, the identity matrix.
+ * - \f$ \overrightarrow{D} = 0_3 \f$, the zero vector.
+ * 
+ * This transfer operation does not change the current LCS.
+ * */
 transfer::transfer () {
   Bt = Mat3::Identity();
   D = Vec3::Zero();
 }
 
+/** The translation vector is initialized to \_D, while the rotation matrix
+ *  \f$ B \f$ is identity.
+ *  @param _D The initial transfer translation vector. */
 transfer::transfer (Vec3 _D) {
-// Identity rotation implied.
   Bt = Mat3::Identity();
   D = _D;
 }
 
+/** The rotation matrix transpose is initialized to \p _Bt, the translation
+ *  vector is initialized to \p _D.
+ *  @param _Bt The initial rotation matrix transpose.
+ *  @param _D The initial translation vector. */
 transfer::transfer (Mat3 _Bt, Vec3 _D) {
   Bt = _Bt;
   D = _D;
@@ -23,7 +34,38 @@ std::ostream& operator<< (std::ostream &out, const transfer &trf) {
   return out;
 }
 
-/* transfer operation */
+/** The operation is decomposed into two successive operations on the ray.
+ * - A change of basis from LCS1 to LCS2.
+ * - A ray intersection with the local plane of LCS2
+ * 
+ * Let the ray coordinates in LCS1, \f$ (P_1, \overrightarrow{V_1}) \f$.
+ * We first operate the change of basis, which gives us ray coordinates
+ * \f$ (P_2, \overrightarrow{V_2}) \f$.
+ *
+ * \f[ \begin{cases}
+ * P_2 &= \begin{bmatrix} x_2 \\ y_2 \\ z_2 \end{bmatrix}
+        = B^{-1} \cdot P_1 - \overrightarrow{D} \\
+   \overrightarrow{V_2} &= \begin{bmatrix} l_2 \\ m_2 \\ n_2 \end{bmatrix}
+                         = B^{-1} \cdot \overrightarrow{V_1}
+ * \end{cases} \f]
+ *
+ * \f$ n_2 = 0 \f$ is an error case, since the ray is parallel to the LCS2
+ * local plane.
+ *
+ * We then operate the intersection of the ray with the LCS2 local plane,
+ * obtaining \f$ (P_3, \overrightarrow{V_3}) \f$.
+ *
+ * \f[ \begin{cases}
+    \overrightarrow{V_3} = \overrightarrow{V_2} \\
+    t = - \frac{z_2}{n_2} \\
+    P_3 = \begin{bmatrix} x_2 + t \cdot l_2 \\
+                          y_2 + t \cdot m_2 \\
+                          0 \end{bmatrix}
+   \end{cases} \f]
+ *
+ * @image html shape/transfer-operation.svg
+ *
+ * */
 void transfer::apply (ray &r) {
   r.v = Bt * r.v;
 
--- src/poaky/src/transfer/transfer.h
+++ src/poaky/src/transfer/transfer.h
@@ -6,19 +6,62 @@
 #include "base/Mat3.h"
 #include "ray/ray.h"
 
-/** @brief Transfer operation over a ray. **/
+/** @brief Transfer operation over a ray.
+ *
+ * The transfer operation defines a new LCS to propagate the ray to. It
+ * operates a change of basis and an intersection with the newly defined local
+ * plane.
+ * Let, in a hypothetical global common coordinate system,
+ * - LCS1 the starting LCS defined by its origin and basis vectors
+ *   \f$ (A_1, \hat{x_1}, \hat{y_1}, \hat{z_1}) \f$
+ * - LCS2 the new LCS defined by the transfer, 
+ *   \f$ (A_2, \hat{x_2}, \hat{y_2}, \hat{z_2}) \f$
+ *
+ * The definition of LCS2 from LCS1 is done with,
+ * - \f$ B \f$: A 3x3 rotation matrix between the basis vectors of LCS1 and
+ *   LCS2.
+ * - \f$ \overrightarrow{D} \f$: A translation vector from LCS1 to LCS2.
+ *
+ * The coordinates of the origin and basis vectors of LCS are expressed with,
+ * \f[ \begin{cases}
+ *   A_2       &= B \cdot \overrightarrow{D} \\
+ *   \hat{x_2} &= B \cdot \hat{x_1} \\
+ *   \hat{y_2} &= B \cdot \hat{y_1} \\
+ *   \hat{z_2} &= B \cdot \hat{z_1}
+ * \end{cases} \f]
+ *
+ * Which is to say, LCS2 is obtained from LCS1 by first applying the rotation
+ * \f$ B \f$ and then translating the origin by \f$ \overrightarrow{D} \f$, with
+ * \f$ D \f$ expressed in the rotated coordinates.
+ * 
+ * \image html shape/transfer-definition.svg
+ * */
+
 class transfer {
   public:
-    Mat3 Bt; // Transpose of rotation matrix B.
-    Vec3 D; // Translation vector D.
+    /** @brief Transpose of the rotation matrix \f$ B \f$.
+     *
+     * Note that since rotation matrices are orthogonal, we have
+     * \f$ B^{-1} = B^\top \f$. */
+    Mat3 Bt;
+    /** @brief Translation vector \f$ D \f$.
+     *
+     * Note the translation from LCS1 to LCS2 is applied in the rotated
+     * coordinates. */
+    Vec3 D;
 
+    /** @brief Default constructor. */
     transfer ();
+    /** @brief Translation initialization constructor. */
     transfer (Vec3 _D);
+    /** @brief Full initialization constructor. */
     transfer (Mat3 _Bt, Vec3 _D);
 
+    /** @brief Apply the transfer to a ray */
     void apply (ray &r);
-};
 
-std::ostream& operator<< (std::ostream &out, const transfer &trf);
+    /** @brief Printer. */
+    friend std::ostream& operator<< (std::ostream &out, const transfer &trf);
+};
 
 #endif // TRANSFER_H
