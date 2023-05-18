82d95857b84da23243677bf876ca13b500dea7d1
thomashoullier <tech@houllier.net>
2023-05-18 07:16:39 +0000

    Deploying to gh-pages from @ thomashoullier/robintrace@04d45c532c297443c357615029c1e7c21f5d061e 🚀

--- search/all_c.js
+++ search/all_c.js
@@ -7,7 +7,7 @@ var searchData=
   ['shape_5freflect_5fpart_49',['shape_reflect_part',['../classshape__reflect__part.html',1,'shape_reflect_part&lt; T &gt;'],['../classshape__reflect__part.html#a5d174ee14db57cfcd62b144529015890',1,'shape_reflect_part::shape_reflect_part()']]],
   ['shape_5frefract_5fpart_50',['shape_refract_part',['../classshape__refract__part.html',1,'shape_refract_part&lt; T &gt;'],['../classshape__refract__part.html#ab82928f1aa757abd280a94b6f697cf32',1,'shape_refract_part::shape_refract_part()']]],
   ['shp_51',['shp',['../classshape__reflect__part.html#a2b4c0aa6a2c6a10b8e6e761889665660',1,'shape_reflect_part::shp()'],['../classshape__refract__part.html#aee05f4a99cbad85cd82d6a5ee57a0c87',1,'shape_refract_part::shp()']]],
-  ['standard_52',['standard',['../classstandard.html',1,'standard'],['../classstandard.html#a98360463d4ea2c2c34040f6bc2c02ee4',1,'standard::standard()'],['../classstandard.html#a0540adb9f0e046c3282605abbf7e4483',1,'standard::standard(double _c, double _k)']]],
+  ['standard_52',['standard',['../classstandard.html',1,'standard'],['../classstandard.html#a98360463d4ea2c2c34040f6bc2c02ee4',1,'standard::standard()'],['../classstandard.html#a0540adb9f0e046c3282605abbf7e4483',1,'standard::standard(double _c, double _k)'],['../classstandard.html#a3eaec19318388b882e4f45802d45c5cf',1,'standard::standard(double _c)']]],
   ['standard_2eh_53',['standard.h',['../standard_8h.html',1,'']]],
   ['states_5ftosave_54',['states_tosave',['../classlseq.html#a77ed7a3e08109933137e5d20e2156009',1,'lseq']]]
 ];
--- search/functions_9.js
+++ search/functions_9.js
@@ -2,5 +2,5 @@ var searchData=
 [
   ['shape_5freflect_5fpart_111',['shape_reflect_part',['../classshape__reflect__part.html#a5d174ee14db57cfcd62b144529015890',1,'shape_reflect_part']]],
   ['shape_5frefract_5fpart_112',['shape_refract_part',['../classshape__refract__part.html#ab82928f1aa757abd280a94b6f697cf32',1,'shape_refract_part']]],
-  ['standard_113',['standard',['../classstandard.html#a98360463d4ea2c2c34040f6bc2c02ee4',1,'standard::standard()'],['../classstandard.html#a0540adb9f0e046c3282605abbf7e4483',1,'standard::standard(double _c, double _k)']]]
+  ['standard_113',['standard',['../classstandard.html#a98360463d4ea2c2c34040f6bc2c02ee4',1,'standard::standard()'],['../classstandard.html#a0540adb9f0e046c3282605abbf7e4483',1,'standard::standard(double _c, double _k)'],['../classstandard.html#a3eaec19318388b882e4f45802d45c5cf',1,'standard::standard(double _c)']]]
 ];
